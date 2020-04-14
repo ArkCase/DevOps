48cae4f3d0cf58449b09e5f0ea2d901cd0bb64a9
Fabrice Triboix <ftriboix@incise.co>
2020-04-14 08:44:32 +0100

    Script to publish files to public S3 buckets

--- mariadb_rotation_lambda/package.sh
+++ mariadb_rotation_lambda/package.sh
@@ -3,24 +3,18 @@
 # Preliminaries
 
 usage() {
-    echo "Usage: AWS_PROFILE=profile $0"
+    echo "Usage: $0"
     echo
-    echo "Make sure you set the AWS_PROFILE environment variable"
-    echo "prior to calling this script."
-    echo
-    echo "Eg: AWS_PROFILE=armedia $0"
+    echo "Create a package for the MariaDB Lambda rotation function"
+    echo "in the form of a zip file."
     exit 1
 }
-
-[ -z "$AWS_PROFILE" ] && usage
 
 set -eu -o pipefail
 
 tmp=$(realpath "$0")
 dir=$(dirname "$tmp")
 cd "$dir"
-
-# Build the package
 
 tmpdir=$(mktemp -d ./pkg-XXXXXXXX)
 pip3 install --system --target "$tmpdir" -r requirements.txt
@@ -29,15 +23,3 @@ zip -r9 ../mariadb_rotation_lambda.zip .
 cd ..
 rm -rf "$tmpdir"
 zip -g mariadb_rotation_lambda.zip mariadb_rotation_lambda.py
-
-# Upload to S3
-
-buckets="armedia-demo-public"
-
-for bucket in $buckets; do
-    aws s3 cp mariadb_rotation_lambda.zip "s3://$bucket/DevOps/mariadb_rotation_lambda.zip"
-done
-
-# Clean up
-
-rm -f mariadb_rotation_lambda.zip
