a9e211aaefcead50fec83f8cdefca198cd5fd509
Fabrice Triboix <ftriboix@incise.co>
2020-07-21 17:04:21 +0100

    Improve security by running Envoy Proxy as regular user instead of root

--- Docker/envoy-tls/envoy-tls/start_envoy.sh
+++ Docker/envoy-tls/envoy-tls/start_envoy.sh
@@ -3,9 +3,13 @@
 set -eu -o pipefail
 
 echo "Creating an X.509 self-signed certificate for $ENVOY_TLS_DOMAIN_NAME"
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/envoy.key -out /etc/ssl/certs/envoy.crt -subj "/CN=$ENVOY_TLS_DOMAIN_NAME"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /app/envoy.key -out /app/envoy.crt -subj "/CN=$ENVOY_TLS_DOMAIN_NAME"
 
-cd /envoy
+cd /app
 cat envoy-config.yaml.tmpl | envsubst > envoy-config.yaml
 
-exec /usr/local/bin/envoy -c /envoy/envoy-config.yaml
+useradd envoy-tls -U -s /bin/bash  # Create custom user and group
+chown envoy-tls.envoy-tls /app/envoy.key /app/envoy.crt
+
+chmod o+w /dev/stdout  # Ensure Envoy Proxy can write to /dev/stdout
+exec execas -u envoy-tls -g envoy-tls -- /usr/local/bin/envoy -c /app/envoy-config.yaml
