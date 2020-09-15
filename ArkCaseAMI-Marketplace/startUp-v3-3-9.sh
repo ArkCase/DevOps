44ff284f7a64817027e5e6aeca614c70e2a2350d
Fabrice Triboix <ftriboix@incise.co>
2020-09-15 18:22:16 +0100

    Fix various problems in the ArkCase AMI startup script

--- ArkCaseAMI-Marketplace/startUp-v3-3-9.sh
+++ ArkCaseAMI-Marketplace/startUp-v3-3-9.sh
@@ -6,32 +6,34 @@ set -eu -o pipefail
 # booted up
 
 if [ ! -e /var/lib/admin-password-changed ]; then
+    sleep 10  # Sometimes, the metadata is not available immediately at boot...
     instance_id=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 
-    pentaho_prop=/opt/arkcase/app/pentaho/pentaho-server/pentaho-solutions/system/applicationContext-security-ldap.properties
+    pentaho_prop=/opt/app/arkcase/app/pentaho/pentaho-server/pentaho-solutions/system/applicationContext-security-ldap.properties
     arkcase_admin_user=$(grep ^adminUser "$pentaho_prop" | sed 's/^adminUser=//')
 
-    alfresco_prop=/opt/arkcase/app/alfresco/shared/classes/alfresco-global.properties
-    ldap_url=$(grep ldap.authentication.java.naming.provider.url "$alfresco_prop" | sed 's/^.*=//')
-    ldap_bind_user=$(grep ldap.synchronization.java.naming.security.principal "$alfresco_prop" | sed 's/^.*=//')
-    ldap_bind_password=$(grep ldap.synchronization.java.naming.security.credentials "$alfresco_prop" | sed 's/^.*=//')
+    alfresco_prop=/opt/app/arkcase/app/alfresco/shared/classes/alfresco-global.properties
+    ldap_url=$(grep ldap.authentication.java.naming.provider.url "$alfresco_prop" | sed 's/^[^=]*=//')
+    ldap_bind_user=$(grep ldap.synchronization.java.naming.security.principal "$alfresco_prop" | sed 's/^[^=]*=//')
+    ldap_bind_password=$(grep ldap.synchronization.java.naming.security.credentials "$alfresco_prop" | sed 's/^[^=]*=//')
 
-    echo "Set password of admin user to the EC2 instance id"
-    password=$(echo -n "$instance_id" | iconv -f utf8 -t utf16le | base64 -w 0)
+    echo "Enable the admin user"
+    sleep 30  # Wait for Samba to be up and running
     tmpfile=$(mktemp /tmp/XXXXXX.ldif)
-    echo "dn: CN=${arkcase_admin_user}" > "$tmpfile"
+    echo "dn: ${arkcase_admin_user}" > "$tmpfile"
     echo "changetype: modify" >> "$tmpfile"
-    echo "replace: unicodePwd" >> "$tmpfile"
-    echo "unicodePwd:: ${password}" >> "$tmpfile"
+    echo "replace: userAccountControl" >> "$tmpfile"
+    echo "userAccountControl: 512" >> "$tmpfile"
     LDAPTLS_REQCERT=never ldapmodify -H "$ldap_url" -D "$ldap_bind_user" -w "$ldap_bind_password" -x -f "$tmpfile"
     rm "$tmpfile"
 
-    echo "Enable the admin user"
+    echo "Set password of admin user to the EC2 instance id"
+    password=$(echo -n '"A$instance_id"' | iconv -f utf8 -t utf16le | base64 -w 0)
     tmpfile=$(mktemp /tmp/XXXXXX.ldif)
-    echo "dn: CN=${arkcase_admin_user},${ldap_user_base}" > "$tmpfile"
+    echo "dn: ${arkcase_admin_user}" > "$tmpfile"
     echo "changetype: modify" >> "$tmpfile"
-    echo "replace: userAccountControl" >> "$tmpfile"
-    echo "userAccountControl:: 512" >> "$tmpfile"
+    echo "replace: unicodePwd" >> "$tmpfile"
+    echo "unicodePwd:: ${password}" >> "$tmpfile"
     LDAPTLS_REQCERT=never ldapmodify -H "$ldap_url" -D "$ldap_bind_user" -w "$ldap_bind_password" -x -f "$tmpfile"
     rm "$tmpfile"
 
@@ -48,31 +50,43 @@ function ref()
     cp -f "$1.orig" "$1"
 }
 
-ref /opt/arkcase/app/alfresco/shared/classes/alfresco/web-extension/share-config-custom.xml
-ref /opt/arkcase/data/arkcase-home/.arkcase/acm/acm-config-server-repo/arkcase.yaml
-ref /opt/arkcase/app/pentaho/pentaho-server/tomcat/conf/server.xml
-ref /opt/arkcase/data/arkcase-home/.arkcase/acm/acm-config-server-repo/arkcase-server.yaml
+ref /opt/app/arkcase/app/alfresco/shared/classes/alfresco/web-extension/share-config-custom.xml
+ref /opt/app/arkcase/data/arkcase-home/.arkcase/acm/acm-config-server-repo/arkcase.yaml
+ref /opt/app/arkcase/app/pentaho/pentaho-server/tomcat/conf/server.xml
+ref /opt/app/arkcase/data/arkcase-home/.arkcase/acm/acm-config-server-repo/arkcase-server.yaml
+
+# Stop services
+
+systemctl stop arkcase
+systemctl stop config-server
+systemctl stop alfresco
+systemctl stop snowbound
+systemctl stop solr
+systemctl stop pentaho
 
 # Modify config files
 
 PublicDNS="$(curl -s http://169.254.169.254/latest/meta-data/public-hostname)"
 pentaho_url1="PENTAHO_SERVER_URL:\ \"https://acm-arkcase\""
 pentaho_url2="PENTAHO_SERVER_URL:\ \"https://$PublicDNS\""
-echo "127.0.0.1 $PublicDNS" >> /etc/hosts
 
-sed -i "s/arkcase-ce.local/$PublicDNS/g" /opt/arkcase/app/alfresco/shared/classes/alfresco/web-extension/share-config-custom.xml
-sed -i "s/$PublicDNS:7070/arkcase-ce.local/g" /opt/arkcase/app/alfresco/shared/classes/alfresco/web-extension/share-config-custom.xml
-sed -i "s~$pentaho_url1~$pentaho_url2~g" /opt/arkcase/data/arkcase-home/.arkcase/acm/acm-config-server-repo/arkcase.yaml
-sed -i "s/arkcase-ce.local/$PublicDNS/g" /opt/arkcase/app/pentaho/pentaho-server/tomcat/conf/server.xml
-sed -i "s/arkcase-ce.local/$PublicDNS/g" /opt/arkcase/data/arkcase-home/.arkcase/acm/acm-config-server-repo/arkcase-server.yaml
-sed -i "3s/$PublicDNS/arkcase-ce.local/g" /opt/arkcase/data/arkcase-home/.arkcase/acm/acm-config-server-repo/arkcase-server.yaml
-sed -i "19s/$PublicDNS/arkcase-ce.local/g" /opt/arkcase/data/arkcase-home/.arkcase/acm/acm-config-server-repo/arkcase-server.yaml
-sed -i "22s/$PublicDNS/arkcase-ce.local/g" /opt/arkcase/data/arkcase-home/.arkcase/acm/acm-config-server-repo/arkcase-server.yaml
-sed -i "58s/$PublicDNS/arkcase-ce.local/g" /opt/arkcase/data/arkcase-home/.arkcase/acm/acm-config-server-repo/arkcase-server.yaml
-sed -i "60s/$PublicDNS/arkcase-ce.local/g" /opt/arkcase/data/arkcase-home/.arkcase/acm/acm-config-server-repo/arkcase-server.yaml
-sed -i "64s/$PublicDNS/arkcase-ce.local/g" /opt/arkcase/data/arkcase-home/.arkcase/acm/acm-config-server-repo/arkcase-server.yaml
-sed -i "101s/$PublicDNS/arkcase-ce.local/g" /opt/arkcase/data/arkcase-home/.arkcase/acm/acm-config-server-repo/arkcase-server.yaml
-sed -i "103s/$PublicDNS/arkcase-ce.local/g" /opt/arkcase/data/arkcase-home/.arkcase/acm/acm-config-server-repo/arkcase-server.yaml
+echo "127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4" > /etc/hosts
+echo "::1         localhost6 localhost6.localdomain6" >> /etc/hosts
+echo "127.0.0.1   $PublicDNS" >> /etc/hosts
+
+sed -i "s/arkcase-ce.local/$PublicDNS/g" /opt/app/arkcase/app/alfresco/shared/classes/alfresco/web-extension/share-config-custom.xml
+sed -i "s/$PublicDNS:7070/arkcase-ce.local/g" /opt/app/arkcase/app/alfresco/shared/classes/alfresco/web-extension/share-config-custom.xml
+sed -i "s~$pentaho_url1~$pentaho_url2~g" /opt/app/arkcase/data/arkcase-home/.arkcase/acm/acm-config-server-repo/arkcase.yaml
+sed -i "s/arkcase-ce.local/$PublicDNS/g" /opt/app/arkcase/app/pentaho/pentaho-server/tomcat/conf/server.xml
+sed -i "s/arkcase-ce.local/$PublicDNS/g" /opt/app/arkcase/data/arkcase-home/.arkcase/acm/acm-config-server-repo/arkcase-server.yaml
+sed -i "3s/$PublicDNS/arkcase-ce.local/g" /opt/app/arkcase/data/arkcase-home/.arkcase/acm/acm-config-server-repo/arkcase-server.yaml
+sed -i "19s/$PublicDNS/arkcase-ce.local/g" /opt/app/arkcase/data/arkcase-home/.arkcase/acm/acm-config-server-repo/arkcase-server.yaml
+sed -i "22s/$PublicDNS/arkcase-ce.local/g" /opt/app/arkcase/data/arkcase-home/.arkcase/acm/acm-config-server-repo/arkcase-server.yaml
+sed -i "58s/$PublicDNS/arkcase-ce.local/g" /opt/app/arkcase/data/arkcase-home/.arkcase/acm/acm-config-server-repo/arkcase-server.yaml
+sed -i "60s/$PublicDNS/arkcase-ce.local/g" /opt/app/arkcase/data/arkcase-home/.arkcase/acm/acm-config-server-repo/arkcase-server.yaml
+sed -i "64s/$PublicDNS/arkcase-ce.local/g" /opt/app/arkcase/data/arkcase-home/.arkcase/acm/acm-config-server-repo/arkcase-server.yaml
+sed -i "101s/$PublicDNS/arkcase-ce.local/g" /opt/app/arkcase/data/arkcase-home/.arkcase/acm/acm-config-server-repo/arkcase-server.yaml
+sed -i "103s/$PublicDNS/arkcase-ce.local/g" /opt/app/arkcase/data/arkcase-home/.arkcase/acm/acm-config-server-repo/arkcase-server.yaml
 
 # Enable and start services
 
