bf254699277fbf301ff219f47787c4d101979fe0
Fabrice Triboix <ftriboix@incise.co>
2020-10-02 07:28:01 +0100

    Fix report-metering: awscli needs the region

--- Packer/test-backend/report-metering.sh
+++ Packer/test-backend/report-metering.sh
@@ -11,15 +11,18 @@ if [ -e "$users_file" ]; then
 fi
 echo "Number of users: $nusers"
 
+token=$(curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+region=$(curl -s -H "X-aws-ec2-metadata-token: $token" http://169.254.169.254/latest/meta-data/placement/availability-zone | sed -e 's/[a-z]$//')
 product_code=$(cat /app/aws-marketplace-product-code)
 timestamp=$(date -u +'%Y-%m-%dT%H:%M:%S')
 dimension=UserCount
 
+echo "Region: $region"
 echo "Product code: $product_code"
 echo "Timestamp: $timestamp"
 echo "Dimension: $dimension"
 
-aws meteringmarketplace meter-usage \
+aws meteringmarketplace --region "$region" meter-usage \
         --product-code "$product_code" \
         --timestamp "$timestamp" \
         --usage-dimension "$dimension" \
