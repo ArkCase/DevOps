5ad32bafafbdb5a00a00098758465086fb217269
Fabrice Triboix <ftriboix@incise.co>
2020-10-12 11:26:43 +0100

    Alfresco Docker: Ensure HAProxy rewrite Location headers
    When queried on `/alfreso`, NGINX redirects to
    `http://localhost:8180/alfresco/`. So we need to configure HAProxy to rewrite
    this `Location` header to something like `http://localhost:10080/alfresco/`.

--- Docker/Alfresco/ark-haproxy/start_haproxy.sh
+++ Docker/Alfresco/ark-haproxy/start_haproxy.sh
@@ -11,7 +11,7 @@ chmod 700 /etc/keys/key.pem
 
 cd /app
 cat haproxy.cfg.tmpl \
-    | envsubst '${ACM_HAPROXY_ACS_REPO_HOST} ${ACM_HAPROXY_ACS_REPO_PORT} ${ACM_HAPROXY_ACS_SHARE_HOST} ${ACM_HAPROXY_ACS_SHARE_PORT}' \
+    | envsubst '${ACM_HAPROXY_DOMAIN_NAME} ${ACM_HAPROXY_HTTP_PORT} ${ACM_HAPROXY_HTTPS_PORT} ${ACM_HAPROXY_ACS_REPO_HOST} ${ACM_HAPROXY_ACS_REPO_PORT} ${ACM_HAPROXY_ACS_SHARE_HOST} ${ACM_HAPROXY_ACS_SHARE_PORT}' \
     > /usr/local/etc/haproxy/haproxy.cfg
 
 exec haproxy -f /usr/local/etc/haproxy/haproxy.cfg
