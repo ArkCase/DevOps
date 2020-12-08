ea49fbeca840636fa2d82e82636abc5504efdbc0
Fabrice Triboix <ftriboix@incise.co>
2020-12-08 16:13:11 +0000

    NGINX proxy fetches valid TLS certificate when running on ECS

--- Docker/Alfresco/nginx-acs/start_nginx.sh
+++ Docker/Alfresco/nginx-acs/start_nginx.sh
@@ -2,11 +2,27 @@
 
 set -eu -o pipefail
 
-echo "Creating an X.509 self-signed certificate"
-mkdir /etc/certs /etc/keys
+mkdir -p /etc/certs /etc/keys
+chown root:root /etc/certs /etc/keys
 chmod 755 /etc/certs
 chmod 700 /etc/keys
-openssl req -x509 -nodes -days 9000 -newkey rsa:2048 -keyout /etc/keys/key.pem -out /etc/certs/cert.pem -subj "/CN=proxy"
+
+if [ -v ARK_KEY_PARAMETER_NAME ]; then
+    echo "I am being run on ECS"
+    echo "Fetching the TLS private key and certificate"
+    aws ssm get-parameter --name "${ARK_CERT_PARAMETER_NAME}" --with-decryption \
+            | jq -r .Parameter.Value > /etc/certs/cert.pem
+    aws ssm get-parameter --name "${ARK_INTERMEDIATE_CERT_PARAMETER_NAME}" --with-decryption \
+            | jq -r .Parameter.Value >> /etc/certs/cert.pem
+    aws ssm get-parameter --name "${ARK_KEY_PARAMETER_NAME}" --with-decryption \
+            | jq -r .Parameter.Value > /etc/keys/key.pem
+else
+    echo "Creating an X.509 self-signed certificate"
+    openssl req -x509 -nodes -days 9000 -newkey rsa:2048 -keyout /etc/keys/key.pem -out /etc/certs/cert.pem -subj "/CN=proxy"
+fi
+
+chown root:root /etc/certs/cert.pem /etc/keys/key.pem
+chmod 755 /etc/certs/cert.pem
 chmod 700 /etc/keys/key.pem
 
 cd /app
