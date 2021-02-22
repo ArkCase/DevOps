958693721d2fc74792fdc30ff6b4e91969c9f2d0
Fabrice Triboix <ftriboix@incise.co>
2021-02-22 11:59:48 +0000

    Catch errors from `backend` in `web` microservice

--- samples/istio-toy-project/docker/web/web.py
+++ samples/istio-toy-project/docker/web/web.py
@@ -1,31 +1,57 @@
 #!/usr/bin/env python3
 
 import os
-from flask import Flask
+from flask import Flask, request, make_response
 import requests
 import json
 
 backend_url = os.environ['BACKEND_URL']
 
+tracing_headers = [
+        "x-request-id",
+        "x-b3-traceid",
+        "x-b3-spanid",
+        "x-b3-parentspanid",
+        "x-b3-sampled",
+        "x-b3-flags",
+        "x-ot-span-context",
+        "x-cloud-trace-context",
+        "traceparent",
+        "grpc-trace-bin"
+]
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    if backend_url.endswith("/"):
+        url = backend_url + "fromweb"
+    else:
+        url = backend_url + "/fromweb"
+
+    headers = {}
+    for header in tracing_headers:
+        headers[header] = request.headers.get(header)
+        if headers[header]:
+            print(f"Header: {header}: {headers[header]}", flush=True)
+
     try:
-        if backend_url.endswith("/"):
-            url = backend_url + "fromweb"
-        else:
-            url = backend_url + "/fromweb"
         print(f"Querying backend: {url}", flush=True)
-        response = requests.get(url)
+        response = requests.get(url, headers=headers)
         print(f"Backend returned status: {response.status_code}", flush=True)
         if response.status_code != 200:
             raise RuntimeError(f"Gateway error")
         print(f"Backend returned message: {response.text}", flush=True)
-        data = json.loads(response.text)['message']
+        status_code = 200
+        msg = json.loads(response.text)['message']
+        data = {'backend': msg}
+
     except Exception as e:
-        data = f"Backend unavailable: {str(e)}"
-    return f"<html><body>Backend returned: {data}</body></html>\n"
+        status_code = 503
+        data = {'reason': str(e)}
+
+    data['status_code'] = status_code
+    return make_response(json.dumps(data), status_code)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8081)
