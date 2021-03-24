d1be8bbd16d2cbde5fd4c0c13751eb6f5297b61c
Fabrice Triboix <ftriboix@incise.co>
2021-03-24 21:15:08 +0000

    Minikube setup: Add Loki

--- kubernetes/minikube/setup-cluster.sh
+++ kubernetes/minikube/setup-cluster.sh
@@ -26,13 +26,42 @@ echo "*** Installing Istio ***"
 istioctl install -y --set profile=$ISTIO_PROFILE
 sleep 10
 
+# Add the `istio-injection=enabled` label to the `default` namespace, so that
+# Istio will automatically inject side-car proxies to any pod created in this
+# namespace.
+kubectl label namespace default istio-injection=enabled
+
 echo
 echo
 echo "*** Installing Calico ***"
 kubectl apply -f calico.yaml
+sleep 10  # Give some time to the controller to create pods
 while true; do
-    tmp=$(kubectl get pods -n kube-system | grep calico-node | awk '{ print }')
-    if [ "$tmp" = "1/1" ]; then
+    tmp=$(kubectl -n kube-system get pods | grep calico-node | awk '{ print $2 }')
+    have=$(echo "$tmp" | cut -d/ -f1)
+    want=$(echo "$tmp" | cut -d/ -f2)
+    if [ "$have" = "$want" ]; then
+        break
+    else
+        sleep 1
+        echo -n .
+    fi
+done
+echo
+sleep 10
+
+echo
+echo
+echo "*** Installing Loki ***"
+kubectl create namespace loki
+kubectl label namespace loki istio-injection=enabled
+helm -n loki install -f loki-values.yaml loki grafana/loki
+sleep 10  # Give some time to the controller to create pods
+while true; do
+    tmp=$(kubectl -n loki get pods | grep loki-0 | awk '{ print $2 }')
+    have=$(echo "$tmp" | cut -d/ -f1)
+    want=$(echo "$tmp" | cut -d/ -f2)
+    if [ "$have" = "$want" ]; then
         break
     else
         sleep 1
