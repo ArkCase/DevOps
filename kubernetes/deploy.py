0e1df3ddd392de4857d6e7d4bdf9e5d070a0ab76
Fabrice Triboix <ftriboix@incise.co>
2021-05-10 10:27:00 +0100

    Kubernetes deploy script: Add wait_for_pod() and Calico

--- kubernetes/deploy.py
+++ kubernetes/deploy.py
@@ -4,19 +4,8 @@ import argparse
 import yaml
 import subprocess
 import sys
-
-
-def run(args):
-    if isinstance(args, str):
-        args = args.split()
-    try:
-        output = subprocess.check_output(args)
-    except subprocess.CalledProcessError as e:
-        print(f"ERROR: Failed to run command: {args}: {str(e)}",
-              file=sys.stderr,
-              flush=True)
-        sys.exit(1)
-    return output.decode("utf-8")
+import os
+import time
 
 
 ############################
@@ -34,19 +23,76 @@ args = ap.parse_args()
 with open(args.CFGFILE) as f:
     cfg = yaml.safe_load(f)
 
+# Go into the "files" sub-directory
+this_file = os.path.abspath(__file__)
+this_dir = os.path.dirname(this_file)
+os.chdir(os.path.join(this_dir, "files"))
 
-def dbg(msg):
+
+#############
+# Functions #
+#############
+
+
+def dbg(msg: str):
     if args.verbose:
         print(f"DEBUG: {msg}", flush=True)
 
 
-def info(msg):
+def info(msg: str):
     print(f"INFO: {msg}", flush=True)
 
 
-def err(msg):
+def err(msg: str):
     print(f"ERROR: {msg}", file=sys.stderr, flush=True)
 
+
+def run(args):
+    """
+    Run a command and return the output. The output returned by the command
+    must be a human-readable string. If the command fails, exits immediately.
+
+    `args` can be either a string or a list. Examples:
+        "kubectl get pods"
+        ["kubectl", "get", "pods"]
+    """
+    if isinstance(args, str):
+        args = args.split()
+    try:
+        output = subprocess.check_output(args)
+    except subprocess.CalledProcessError as e:
+        raise RuntimeError(f"Failed to run command: {args}: {str(e)}")
+    return output.decode("utf-8")
+
+
+def wait_for_pod(start_of_podname: str,
+                 namespace: str = "default",
+                 timeout_seconds: int = 600):
+    start_time = int(time.time())
+    time.sleep(10)  # Give some time to the controller to create pods
+    while int(time.time()) - start_time <= timeout_seconds:
+        output = run(["kubectl", "-n", namespace, "get", "pods"])
+        lines = output.splitlines()[1:]  # Remove header line
+        for line in lines:
+            if line.startswith(start_of_podname):
+                # We found the pod. Now check whether all its containers are
+                # started.
+                tmp = line.split()[1]  # Extract containers number: "2/3"
+                tmp = tmp.split("/")
+                have = int(tmp[0])
+                want = int(tmp[1])
+                if have == want:
+                    # All containers are stared, hooray!
+                    sys.stdout.write("\n")
+                    sys.stdout.flush()
+                    return
+        sys.stdout.write(".")
+        sys.stdout.flush()
+        time.sleep(2)
+    raise RuntimeError(f"Pod {start_of_podname} not started in {timeout_seconds} seconds")
+
+
+# Helm repos
 
 info("")
 info("*** Adding Helm repositories ***")
@@ -66,6 +112,9 @@ add_helm_repo("prometheus-community",
 add_helm_repo("kube-state-metrics",
               "https://kubernetes.github.io/kube-state-metrics")
 
+
+# Cluster-wide stuff
+
 info("")
 info("*** Setting up cluster-wide stuff ***")
 
@@ -79,3 +128,11 @@ if not exists:
     run("kubectl create namespace observability")
 run("kubectl label namespace default istio-injection=enabled --overwrite")
 run("kubectl label namespace observability istio-injection=enabled --overwrite")
+
+
+# Calico
+
+info("")
+info("*** Installing/Updating Calico ***")
+run("kubectl apply -f calico.yaml")
+wait_for_pod("calico-node", "kube-system")
